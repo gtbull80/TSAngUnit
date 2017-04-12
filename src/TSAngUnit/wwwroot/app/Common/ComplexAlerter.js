f125154847d1ec29acfe5b74336e5a2e4a97f8ba
gtbull80 <gtbull80@gmail.com>
2017-04-12 18:10:17 -0400

    removing generated js files from source control

--- src/TSAngUnit/wwwroot/app/Common/ComplexAlerter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var ComplexNumber_1 = require("./ComplexNumber");
-$(function () {
-    var cn = new ComplexNumber_1.ComplexNumber(2, 4);
-    alert("real: " + cn.real + "-----imaginary: " + cn.imaginary);
-});
-//# sourceMappingURL=ComplexAlerter.js.map
